14625d9b3b33d33f65ec4cd2c74641873e215ed4
RA57-2018 <bugarski.ra57.2018@uns.ac.rs>
2022-05-31 19:28:07 +0200

    feat:#1 initialisation model, service, controller and repository for user

--- backend/PswProject/PswProject/Program.cs
+++ backend/PswProject/PswProject/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PswProject.dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
